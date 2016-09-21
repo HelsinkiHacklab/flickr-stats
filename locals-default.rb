3e89f42f983ed13ca546903cbf5d7c9d4039759e
jssmk <jussmaki@fastmail.fm>
2016-09-21 22:38:16 +0300

    default local settings example

---
+++ locals-default.rb
@@ -0,0 +1,27 @@
+#
+# Set your local options using this file
+#
+
+# Select which group pics to download
+
+# example, hacklab.fi: MY_group_id = "2892234@N22"
+MY_group_id = "000000@N22"
+
+# easy way to get your group_id using the API
+MY_user_name_url = "https://www.flickr.com/people/you"
+# example: MY_user_name_url = "https://www.flickr.com/people/helsinkihacklab/"
+
+# use a proxy if you need to
+MY_proxy = ""
+
+# Local paths
+
+CSV_path    = HOME_path+"your/path/to/csv/file/out.csv"
+
+# Flickr keys
+MY_api_key="00000000000000000000000000000000"
+MY_shared_secret="0000000000000000"
+
+MY_access_token = "0000000000000000000000000000000000"
+MY_access_secret = "0000000000000000"
+
