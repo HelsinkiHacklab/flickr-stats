3c8892441d007f11b9aa52fae165b06d12a1c9db
jssmk <jussmaki@fastmail.fm>
2016-09-27 20:13:09 +0300

    Make graph polar

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # flickr-stats
 Draws graphs based on flickr photo upload frequency
+
+## Requirements
+
+R 3.0.+ and libraries:
+ * Cairo (libcairo-dev required)
+ * data.table
+ * ggplot2
+
+Ruby2 and libraries
+ * flickraw
+
--- draw_graph.R
+++ draw_graph.R
@@ -7,22 +7,37 @@ Cairo()
 data = fread("example_output.csv")
 for (x in 0:(nrow(data)-1)) {data$hours_end[x+1] = data$hours[(x+1)%%nrow(data)+1] }
 
-data <- data[order(factor(weekday, levels = c("Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday")),hours),]
+weekdays_en <- c("Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday")
+
+data <- data[order(factor(weekday, levels = weekdays_en),hours),]
 
 data$timeline <- paste0(data$hours, " - ",  data$hours_end, " ", data$weekday)
 
 
-freq_table <- data[, list(mean(freq)), by="timeline"]
-colnames(freq_table) <- c("timeline", "mean")
+freq_table <- data[, list(median(freq)), by="timeline"]
+freq_table <- data[, list(median(freq), quantile(freq, .25), quantile(freq, .75)), by="timeline"]
+
+setnames(freq_table, c("timeline", "median", "p25", "p75"))
 
 freq_table$timeline <- factor(freq_table$timeline, levels=freq_table$timeline) # preserve order
 
-print(freq_table)
+# hardcoded axis limits could be changed in the future
 
-area_plot <- ggplot(freq_table, aes(x=timeline, y=mean, group=1))+geom_area()+geom_vline(xintercept=c(4.5, 8.5, 12.5, 16.5, 20.5, 24.5))#+coord_polar()
+polar_plot <- ggplot(freq_table) + theme_bw() + geom_blank() +
+  scale_x_discrete(expand=c(0,0)) +
+  labs(x="Median ± 25 %") +
+  theme(axis.title.x=element_text(family = "Georgia", color="#303030", face="italic", size=12, hjust=0),
+        axis.text.x=element_blank(),
+        axis.title.y=element_blank(),
+        panel.grid.major = element_line(colour = "#0000FF50"),
+        plot.title = element_text(family = "Georgia", color="#000000", size=18)) +
+  geom_area(aes(x=timeline, y=p75, group=1), fill="#FF000040") +
+  geom_line(aes(x=timeline, y=median, group=1), size=1, color="#FF0000FF") +
+  geom_area(aes(x=timeline, y=p25, group=1), fill="#FFFFFFFF") +
+  geom_vline(xintercept=c(0, 4, 8, 12, 16, 20, 24)) +
+  annotate("text",label = weekdays_en, x = c(2,6,10,14,18,22,26), y=rep(15,7)) +
+  coord_polar() +
+  scale_y_sqrt(limits=c(0,15)) +
+  ggtitle(paste0("Helsinki Hacklab flickr upload activity\n",min(data$date_taken), " — ", max(data$date_taken)))
 
-
-
-ggsave(file="weekly.png", type="cairo-png")
-
-
+ggsave(file="weekly_polar.png", type="cairo-png")
